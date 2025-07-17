26e6e69344abbf499c3a3f6bdb5d11c955ae1fdd
Saerom Park <park.srome@gmail.com>
2025-07-16 23:29:06 -0500

    serve react app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, '../../web/dist')));
+
+app.get('/api', (req, res) => {
   res.send('Hello from the API!');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../web/dist/index.html'));
 });
 
 app.listen(port, () => {
